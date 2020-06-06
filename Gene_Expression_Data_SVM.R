22d05835ea545fa91e07c222529cfd5d0f048975
Abdur Rahman <airdipu@gmail.com>
2020-06-06 16:17:29 +0200

    Application to Gene Expression Data

---
+++ Gene_Expression_Data_SVM.R
@@ -0,0 +1,17 @@
+# Application to Gene Expression Data
+
+library(ISLR)
+names(Khan)
+dim(Khan$xtrain)
+dim(Khan$xtest)
+length(Khan$ytrain)
+length(Khan$ytest)
+table(Khan$ytrain)
+table(Khan$ytest)
+dat=data.frame(x=Khan$xtrain, y=as.factor(Khan$ytrain))
+out=svm(y~., data=dat, kernel="linear",cost=10)
+summary(out)
+table(out$fitted, dat$y)
+dat.te=data.frame(x=Khan$xtest, y=as.factor(Khan$ytest))
+pred.te=predict(out, newdata=dat.te)
+table(pred.te, dat.te$y)
